63ec35ad8255e4b182239bf8085a09fc2c603093
Jérémi <Kelhen@users.noreply.github.com>
2019-11-24 21:39:57 +0900

    add routes to new pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+import Apple from "../views/Apple.vue";
+import Instagram from "../views/Instagram.vue";
 
 Vue.use(VueRouter);
 
@@ -12,9 +13,16 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    component: About
+    path: "/apple",
+    name: "apple",
+    component: Apple,
+    meta: { layout: 'apple' },
+  },
+  {
+    path: "/instagram",
+    name: "instagram",
+    component: Instagram,
+    meta: { layout: 'instagram' },
   }
 ];
 
